21752791e586cde381afa8d945f24d30805f5752
possiblyHugo <alainhandzar@gmail.com>
2024-02-12 01:49:02 -0800

    + Fixed getline not working because I didn't clear the stream buffer

--- Day1/Day1/main.cpp
+++ Day1/Day1/main.cpp
@@ -4,20 +4,20 @@
 
 int main() {	
 	std::vector<std::string> calibration_document;
-	std::string single_line_buffer = "";
 	int lineCount = 0;
 
 	std::cout << "Calibration Document line count:  " << std::endl;
 	std::cin >> lineCount;
 
 	for (int i = 0; i < lineCount; i++) {
+		std::string single_line_buffer;
 		
 		std::cout << "Please input line " << i + 1 << std::endl; // + 1 is there to not display (line 1) or the line being off by 1.
+		std::cin.clear();
+		std::cin.ignore(1);
 		std::getline(std::cin, single_line_buffer);
 
 		calibration_document.push_back(single_line_buffer);
-		std::cin.clear();
-		std::cin.ignore(1);
 	}
 
 	for (int i = 0; i < lineCount; i++) {
